637f0373ec02defe1eb182cb915407d3895ec2f7
Hayat A <xaetbek@gmail.com>
2025-05-06 09:26:54 -0700

    Update Room.java
    Modify the Room class to add checkIn(), checkout() and cleanroom() methods to the room.
    
    Once a room has been checked in, it should be occupied and marked as dirty. When a guest checks out of a room it must first be cleaned by a housekeeper before another guest can check into the room.

--- HotelOperations/src/main/java/org/pluralsight/Room.java
+++ HotelOperations/src/main/java/org/pluralsight/Room.java
@@ -3,16 +3,58 @@ package org.pluralsight;
 public class Room {
     private int numberOfBeds;
     private double price;
-    private boolean occupied;
-    private boolean dirty;
-    private boolean available;
+    private boolean isOccupied;
+    private boolean isDirty;
+    private boolean isAvailable;
 
-    public Room(int numberOfBeds, double price, boolean occupied, boolean dirty, boolean available) {
+    public Room(int numberOfBeds, double price, boolean isOccupied, boolean isDirty, boolean isAvailable) {
         this.numberOfBeds = numberOfBeds;
         this.price = price;
-        this.occupied = occupied;
-        this.dirty = dirty;
-        this.available = available;
+        this.isOccupied = isOccupied;
+        this.isDirty = isDirty;
+        this.isAvailable = isAvailable;
+    }
+
+    public Room() {
+        this.numberOfBeds = 0;
+        this.price = 0.0;
+        this.isOccupied = false;
+        this.isDirty = false;
+        this.isAvailable = true;
+    }
+
+    public void checkIn() {
+        if(!isOccupied && !isDirty && isAvailable) {
+            isOccupied = true;
+            isDirty = true;
+            isAvailable = false;
+            System.out.println("Guest has checked in.");
+        } else {
+            System.out.println("Room is not ready for check-in.");
+        }
+    }
+
+    public void checkOut() {
+        if(isOccupied) {
+            isOccupied = false;
+            isDirty = true;
+            isAvailable = false;
+            System.out.println("Guest has checked out. Room is now dirty.");
+        } else {
+            System.out.println("The room is already unoccupied.");
+        }
+    }
+
+    public void cleanRoom() {
+        if(!isOccupied && isDirty){
+            isDirty = false;
+            isAvailable = true;
+            System.out.println("Room has been cleaned and is available.");
+        } else if (isOccupied) {
+            System.out.println("Can't clean room while it's occupied.");
+        } else {
+            System.out.println("Room is already clean.");
+        }
     }
 
     public int getNumberOfBeds() {
@@ -24,15 +66,15 @@ public class Room {
     }
 
     public boolean isOccupied() {
-        return occupied;
+        return isOccupied;
     }
 
     public boolean isDirty() {
-        return dirty;
+        return isDirty;
     }
 
     public boolean isAvailable() {
-        return available;
+        return isAvailable;
     }
 
 }
