d1e49204c8c6a4e780a02bb7048d426b73e38d40
Hayat A <xaetbek@gmail.com>
2025-05-07 09:56:23 -0700

    Create Hotel.java class
    This class will allow you to create a simple hotel object and check if there are rooms available for the current day only. The hotel has 2 types of rooms a King Suite and a Basic Double room.

---
+++ HotelOperations/src/main/java/org/pluralsight/Hotel.java
@@ -0,0 +1,31 @@
+package org.pluralsight;
+
+public class Hotel {
+    private String name;
+    private int numberOfSuites;
+    private int numberOfRooms;
+    private int bookedSuites;
+    private int bookedBasicRooms;
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = 0;
+        this.bookedBasicRooms = 0;
+    }
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedSuites , int bookedBasicRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = bookedSuites;
+        this.bookedBasicRooms = bookedBasicRooms;
+    }
+
+    public boolean bookRoom(int numberOfRooms, boolean isSuite) {
+    }
+
+    public int getAvailableSuites(){}
+    public int getAvailableRooms(){}
+}
