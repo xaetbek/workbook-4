5433c53c0a4aa93cb85c60bf3b3fed70c93f417b
Hayat A <xaetbek@gmail.com>
2025-05-08 13:39:41 -0700

    Create and EmployeeTest class
    Create and EmployeeTest class to test the punchIn and punchOut methods of the Employee class. Verify that when an employee punches out, their time is calculated correctly.
    
    punchIn(double time) punchOut(double time)

--- HotelOperations/src/test/java/org/pluralsight/EmployeeTest.java
+++ HotelOperations/src/test/java/org/pluralsight/EmployeeTest.java
@@ -1,7 +1,33 @@
 package org.pluralsight;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
+    @Test
+    public void punchInTest() {
+        // Arrange
+        Employee employee = new Employee(1, "Adam", "IT", 80, 0);
 
+        // Act
+        employee.punchIn(9);
+        employee.punchOut(19);
+
+        // Assert
+        assertEquals(10, employee.getHoursWorked(), 0.01, "Employee should have worked 10 hours");
+    }
+
+    @Test
+    public void punchOutTest() {
+        // Arrange
+        Employee employee = new Employee(1, "Adam", "IT", 80, 0);
+
+        // Act
+        employee.punchOut(17.0);  // Should do nothing since not clocked in
+
+        // Assert
+        assertEquals(0.0, employee.getHoursWorked(), 0.01,
+                "Hours worked should remain 0 if not clocked in");
+    }
 }
